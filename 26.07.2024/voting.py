93e02719d6a3dedbf422e379174d5b4ebfa6c933
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-07-26 22:03:23 +0530

    Create voting.py

---
+++ 26.07.2024/voting.py
@@ -0,0 +1,8 @@
+PROGRAM: VOTING ELIGIBILITY
+a=int(input("Enter the age of the person"))
+if(a>=18):
+  print("Person is eligible to vote")
+else:
+  print("Person is not eligible to vote")
+=======================================================================================
+
