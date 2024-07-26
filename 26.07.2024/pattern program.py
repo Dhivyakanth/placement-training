854195b2819dafd7a252bf5135ccc2987857deb9
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-07-26 22:01:54 +0530

    Create pattern program.py

---
+++ 26.07.2024/pattern program.py
@@ -0,0 +1,6 @@
+n=int(input("Enter a number of rows : "))
+for i in range(n+1):
+    for j in range(i):
+        print(i,end=" ")
+    print()
+=================================================================================================
