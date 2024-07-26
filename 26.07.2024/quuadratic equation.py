13a69a46536a9d949a78e9216698dad4412e5be2
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-07-26 21:56:10 +0530

    Create quuadratic equation.py

---
+++ 26.07.2024/quuadratic equation.py
@@ -0,0 +1,11 @@
+ PROGRAM: QUADRATIC EQUATION
+ a=int(input("Enter the coeffecient 1:")) 
+ print(a) 
+ b=int(input("Enter the coeffecient 2:")) 
+ print(b) 
+ c=int(input("Enter the coeffecient 3:")) 
+ print(c) 
+ root1=((b*2)-(4*a*c))*0.5 
+ root2=((b*2)-(4*a*c))*0.5 
+ print((-b+root1)/(2*a),(-b-root2)/(2*a))
+================================================================================================
