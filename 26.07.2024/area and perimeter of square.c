d27957995757561a6925571948e01dc03006e5bb
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-07-27 19:40:43 +0530

    Create area and perimeter of square.c

---
+++ 26.07.2024/area and perimeter of square.c
@@ -0,0 +1,13 @@
+// c program to find area and perimeter of square
+#include<stdio.h>
+int main()
+{
+   int a,area,perimeter;
+   printf("\nEnter a:");
+   scanf("%d", &a);
+   area = a*a;
+   perimeter = 4*a;
+   printf("Area of Square : %d", are);
+   printf("Perimeter of Square : %d", perimeter);
+}
+
