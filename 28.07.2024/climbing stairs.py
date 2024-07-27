d8961fc8ddda93ae1e9327517a40eccc2583185e
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-07-27 21:23:28 +0530

    Update climbing stairs.py

--- 28.07.2024/climbing stairs.py
+++ 28.07.2024/climbing stairs.py
@@ -1,8 +1,8 @@
 class Solution:
-    def climbStairs(self, n: int) -> int:
+    def climbStairs(self, n):
 
-        if x<=2:
-            return x
+        if n<=2:
+            return n
         else:
             a,b,c=1,2,0
             for i in range(2,n):
