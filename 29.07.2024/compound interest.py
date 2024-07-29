37de4ff5bafbb6a13a0dcfb5141adee598988c57
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-07-29 22:32:30 +0530

    Create compound interest.py

---
+++ 29.07.2024/compound interest.py
@@ -0,0 +1,7 @@
+principal = int(input("Enter the principal amount: "))
+rate = int(input("Enter rate of interest: "))
+time = int(input("Enter time in years: " ))
+Amount = principal * (pow((1 + rate / 100), time))
+CI = Amount - principal
+print("Compound interest is", CI)
+ 
