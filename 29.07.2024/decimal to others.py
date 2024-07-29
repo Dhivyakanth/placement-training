59cf3aa23a38ed63f9f88cefded88cf2500165da
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-07-29 22:34:44 +0530

    Create decimal to others.py

---
+++ 29.07.2024/decimal to others.py
@@ -0,0 +1,4 @@
+a=int(input("Enter the decimal number:")
+print(f"The binary value of {a} is:",bin(a))
+print(f"The octal value of {a} is:",oct(a))
+print(f"The hexadecimal value of {a} is:",hex(a))
