16f31984b4e472d619faf965b75c6ec7be338948
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-08-01 21:58:51 +0530

    Create add.java

---
+++ 01.08.2024/add.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;  
+class Main 
+{  
+public static void main(String[] args)  
+{  
+Scanner sc= new Scanner(System.in);   
+System.out.println("Enter first number");  
+int a= sc.nextInt();  
+System.out.println("Enter second number");  
+int b= sc.nextInt();  
+System.out.println("Enter third number");  
+int c= sc.nextInt();  
+System.out.println("Total= " +(a+b+c));
+}
+}
