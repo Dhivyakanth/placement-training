89ea63560c03b09734daa975c6caef298a83e6d4
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-08-01 22:03:18 +0530

    Create for loop and while loop.java

---
+++ 01.08.2024/for loop and while loop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;  
+class Main 
+{  
+public static void main(String[] args)  
+{  
+Scanner sc= new Scanner(System.in);   
+System.out.println("Enter first number");  
+int n= sc.nextInt();
+for(int i=1;i<=10;i+=2)  
+    System.out.println(i);
+while(n<=10)
+    System.out.println(n);  
+    n++;  
+}  
+}  
