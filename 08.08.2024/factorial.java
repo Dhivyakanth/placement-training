dc5c878fdb3d0cd90a476392e0a5eb3f1fbb8853
Dhivyakanth <147681324+Dhivyakanth@users.noreply.github.com>
2024-08-08 21:03:45 +0530

    Create factorial.java

---
+++ 08.08.2024/factorial.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class factorial
+{
+    public static void main(String []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number:");
+        int n=sc.nextInt();
+        int fact=1;
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("The factorial of "+n+" is "+fact);
+}}
